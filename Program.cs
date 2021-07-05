9128ce79224f4fae047b9cd0d9411cc387757383
Angelo Breuer <angelobr@outlook.de>
2021-07-05 14:50:10 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Parsing;
+
+return new CommandLineBuilder()
+    .UseResources(new MyResources())
+    .UseDefaults()
+    .Build()
+    .Invoke("--help");
+
+class MyResources : Resources
+{
+    public override string HelpUsageTile()
+    {
+        throw new Exception("Never called :(");
+    }
+}
